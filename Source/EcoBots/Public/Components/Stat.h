40e550a71891761b4d531a4a6c7af1c630ff58c2
Emanuele Pardini <66436200+EmanuelePardini@users.noreply.github.com>
2024-06-24 01:43:13 +0200

    Divided stat struct from statscomponent logic

---
+++ Source/EcoBots/Public/Components/Stat.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "Stat.generated.h"
+
+USTRUCT(BlueprintType)
+struct FStat
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	FName StatName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float MaxValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float CurrentValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+	float PercentValue;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timer")
+	float DecrementDelay;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timer")
+	float DecrementTimer;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Decrement")
+	float DecrementAmount;
+	
+	
+
+	FStat()
+		: StatName("Stat")
+		, MaxValue(1000.f)
+		, CurrentValue(1000.f)
+		, PercentValue(1.f)
+		, DecrementDelay(60.f)
+		, DecrementTimer(0.f)
+		, DecrementAmount(-50.f)
+	{
+	}
+};
